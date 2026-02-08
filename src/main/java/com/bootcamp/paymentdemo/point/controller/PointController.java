7ea1acaae20c13bb7e36cabcefb28933016577df
Soomin <soomin02094@gmail.com>
2026-02-08 14:16:24 +0900

    refactor: PointController 로그 추가

--- src/main/java/com/bootcamp/paymentdemo/point/controller/PointController.java
+++ src/main/java/com/bootcamp/paymentdemo/point/controller/PointController.java
@@ -3,6 +3,7 @@ package com.bootcamp.paymentdemo.point.controller;
 import com.bootcamp.paymentdemo.point.dto.PointGetResponse;
 import com.bootcamp.paymentdemo.point.service.PointService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/points")
@@ -23,6 +25,7 @@ public class PointController {
     // 포인트 내역 조회
     @GetMapping
     public ResponseEntity<List<PointGetResponse>> getPointHistory(Principal principal) {
+        log.info("포인트 내역 조회 요청: email={}", principal.getName());
         return ResponseEntity.status(HttpStatus.OK).body(pointService.getPointHistory(principal.getName()));
     }
 }
