b32e50a67e022487602960eb2bd8cc06c30e2543
JJK3187 <truestory14@kakao.com>
2026-02-08 19:21:21 +0900

    feat: OrderStatus enum

---
+++ src/main/java/com/bootcamp/paymentdemo/order/consts/OrderStatus.java
@@ -0,0 +1,18 @@
+package com.bootcamp.paymentdemo.order.consts;
+
+import lombok.Getter;
+
+@Getter
+public enum OrderStatus {
+    PENDING_PAYMENT("PENDING_PAYMENT"), // 결제 대기
+    PENDING_CONFIRMATION("PENDING_CONFIRMATION"), // 주문 확정 대기
+    CONFIRMED("CONFIRMED"), // 주문 확정
+    CANCELLED("CANCELLED"); // 주문 취소
+
+    private final String OrderStatusName;
+    OrderStatus(String OrderStatusName) {this.OrderStatusName = OrderStatusName;}
+
+    public boolean canBeCancelled() {
+       return this != CONFIRMED && this != CANCELLED;
+    }
+}
