1c03e884d733f82bf8905f1d52e591b93d7288a9
Soomin <soomin02094@gmail.com>
2026-02-11 19:30:42 +0900

    refactor: PaymentController ApiResponse 적용

--- src/main/java/com/bootcamp/paymentdemo/payment/controller/PaymentController.java
+++ src/main/java/com/bootcamp/paymentdemo/payment/controller/PaymentController.java
@@ -1,11 +1,14 @@
 package com.bootcamp.paymentdemo.payment.controller;
 
+import com.bootcamp.paymentdemo.common.dto.ApiResponse;
 import com.bootcamp.paymentdemo.payment.dto.PaymentConfirmResponse;
 import com.bootcamp.paymentdemo.payment.dto.PaymentCreateRequest;
 import com.bootcamp.paymentdemo.payment.dto.PaymentCreateResponse;
 import com.bootcamp.paymentdemo.payment.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,18 +21,22 @@ public class PaymentController {
 
     // 1. 결제 준비 (장부 생성)
     @PostMapping("/create")
-    public PaymentCreateResponse createPayment(@RequestBody PaymentCreateRequest request) {
+    public ResponseEntity<ApiResponse<PaymentCreateResponse>> createPayment(@RequestBody PaymentCreateRequest request) {
         log.info("결제 생성 요청: OrderID={}", request.getOrderId());
-        return paymentService.createPayment(request);
+        PaymentCreateResponse response = paymentService.createPayment(request);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(ApiResponse.success(HttpStatus.CREATED, "결제 생성 성공", response));
     }
 
     // 2. 결제 확정 (검증 및 상태 변경)
     @PostMapping("/{dbPaymentId}/confirm")
-    public PaymentConfirmResponse confirmPayment(
+    public ResponseEntity<ApiResponse<PaymentConfirmResponse>> confirmPayment(
             @PathVariable String dbPaymentId,
             @RequestParam String paymentId) { // 포트원의 imp_uid를 파라미터로 받음
 
         log.info("결제 확정 요청: DB_ID={}, PortOne_ID={}", dbPaymentId, paymentId);
-        return paymentService.confirmPayment(dbPaymentId, paymentId);
+        PaymentConfirmResponse response = paymentService.confirmPayment(dbPaymentId, paymentId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(ApiResponse.success(HttpStatus.OK, "결제 확정 성공", response));
     }
 }
