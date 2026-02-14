8b41b4627a42cd13846b3baef4f06736674545e6
Lee Hanbi <hanbi6717@naver.com>
2026-02-15 00:59:20 +0900

    test: AuthCotroller 테스트 작성 및 PaymentDemoApplication에서 JpaAuditing 어노테이션 분리

--- src/main/java/com/bootcamp/paymentdemo/PaymentDemoApplication.java
+++ src/main/java/com/bootcamp/paymentdemo/PaymentDemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableJpaAuditing
 @EnableScheduling
 public class PaymentDemoApplication {
 
